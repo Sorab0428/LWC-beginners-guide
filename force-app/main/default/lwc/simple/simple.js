4bb59df93dd216d5fce3f1aa9bc5dae34c72f670
Adonis <adonis27100@gmail.com>
2024-05-15 14:27:48 +0800

    change cat picture url

--- force-app/main/default/lwc/simple/simple.js
+++ force-app/main/default/lwc/simple/simple.js
@@ -4,7 +4,7 @@ export default class simple extends LightningElement {
   // Variables used in HTML are declared here
   name = "catName";
   description = "A cute little cat";
-  pictureUrl = "\cat.jpeg";
+  pictureUrl = "https://github.com/Sorab0428/LWC-beginners-guide/blob/master/cat.jpeg?raw=true";
   tag1 = "picture";
   tag2 = "cute";
   tag3 = "cat";
